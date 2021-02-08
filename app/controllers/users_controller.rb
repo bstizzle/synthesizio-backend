654180115221ef57698ad794424a263033ed9e6e
bstizzle <whereyouben@gmail.com>
2021-02-08 15:27:56 -0500

    fixed user permitted params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -29,6 +29,6 @@ class UsersController < ApplicationController
     private
 
     def user_params
-        params.permit(:username, :password_digest)
+        params.permit(:username, :password)
     end
 end
