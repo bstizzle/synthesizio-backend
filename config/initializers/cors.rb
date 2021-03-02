73de73e65a476932fcdaa8ab6b5f94f537ea761f
bstizzle <whereyouben@gmail.com>
2021-03-02 17:22:59 -0500

    cors.rb

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,16 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
-      origins 'http://localhost:3001', 'https://synthesizio.herokuapp.com/'
+      origins 'http://localhost:3001'
+  
+      resource '*',
+        headers: :any,
+        methods: [:get, :post, :put, :patch, :delete, :options, :head],
+        credentials: true
+    end
+
+    allow do
+      origins 'https://synthesizio.herokuapp.com'
   
       resource '*',
         headers: :any,
